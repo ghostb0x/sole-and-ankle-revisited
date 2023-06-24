ec2504b419f1c08f1216475ca33da27172321046
ghostb0x <ghostb0x.github@gmail.com>
2023-06-25 01:40:00 +0200

    Exercise 3 - updated nav and header on tablet and mobile

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -14,15 +14,25 @@ const ShoeIndex = ({ sortId, setSortId }) => {
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
+          <diV>
+            <MobileBreadcrumbs>
+              <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+              <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+              <Breadcrumbs.Crumb href="/sale/shoes">
+                Shoes
+              </Breadcrumbs.Crumb>
+            </MobileBreadcrumbs>
+            <Title>Running</Title>
+          </diV>
+
+          <StyledSelect
             label="Sort"
             value={sortId}
             onChange={(ev) => setSortId(ev.target.value)}
           >
             <option value="newest">Newest Releases</option>
             <option value="price">Price</option>
-          </Select>
+          </StyledSelect>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
@@ -51,6 +61,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +75,31 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media ${QUERIES.tabletAndDown} {
+    align-items: flex-end;
+  }
+
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
+
+const StyledSelect = styled(Select)`
+  @media ${QUERIES.mobileAndDown} {
+    display: none;
+  }
+`;
+
+const MobileBreadcrumbs = styled(Breadcrumbs)`
+  display: none;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: flex;
+    flex-wrap: nowrap;
+  }
 `;
 
 export default ShoeIndex;
