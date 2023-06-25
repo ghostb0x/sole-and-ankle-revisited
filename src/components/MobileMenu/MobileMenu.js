fe210a16c9024bf1c256a64972f88ecaae8aed80
ghostb0x <ghostb0x.github@gmail.com>
2023-06-25 16:24:26 +0200

    exercise 4 - added reach dialog modal and styled mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -2,34 +2,106 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
-
+import { COLORS, WEIGHTS } from '../../constants';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+    >
+      <Content>
+        <ButtonWrapper>
+          <UnstyledButton onClick={() => onDismiss()}>
+            <Icon
+              id="close"
+              strokeWidth={2}
+              color={COLORS.gray[900]}
+              size={24}
+            />
+          </UnstyledButton>
+        </ButtonWrapper>
+
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
 
 export default MobileMenu;
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: hsl(0deg 0% 0% / 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled(DialogContent)`
+  width: 80%;
+  height: 100%;
+  margin-left: auto;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 24px;
+  margin: 26px 16px;
+  margin-left: auto;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 0 50px;
+`;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  text-transform: uppercase;
+  color: ${COLORS.gray[900]};
+  font-size: calc((18 / 16) * 1rem);
+  font-weight: ${WEIGHTS.medium};
+  margin: 11px 0;
+
+  &:first-of-type {
+    color: ${COLORS.secondary}
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 10px 50px;
+`;
+
+const FooterLink = styled.a`
+  text-decoration: none;
+  color: ${COLORS.gray[700]};
+  font-size: calc((14 / 16) * 1rem);
+  margin: 7px 0; 
+`;
